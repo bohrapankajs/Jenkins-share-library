4ba8f22f7aafd363ad0a1fe5b88df4edafb823e4
Pankajbohra <bohrapankajs@gmail.com>
2023-06-02 13:27:02 +0530

    test

---
+++ vars/maven.groovy
@@ -0,0 +1,42 @@
+def lintchecks() {
+    sh " mvn checkstyle:check || true"
+    sh "Lint Check completed $COMPONENT"
+}
+
+def call() {
+    pipeline{
+        agent any
+        environment {
+            SONAR    = credentials('SONAR')
+            // NEXUS    = credentials('NEXUS')
+            SONARURL = "172.31.0.59"
+            // NEXUSURL = "172.31.2.247"
+        }
+        stages {
+            stage('Lint Check') {
+                steps {
+                    script {
+                        lintchecks()
+                    }
+
+                } 
+            }  
+            stage('Sonat Check') {
+                steps {
+                    script {
+                        sh "mvn clean compile"
+                        env.ARGS="-Dsonar.java.binaries=target/"
+                        common.sonarchecks()
+                    }
+                } 
+            } 
+            stage('XYZ Check') {
+                steps {
+                    script {
+                        sh "echo xyz checks"
+                    }
+                } 
+            } 
+        } // end of stages
+    } // end of pipeline
+}
---
+++ vars/python.groovy
@@ -0,0 +1,41 @@
+def lintchecks() {
+        sh "Lint Check completed $COMPONENT"
+}
+
+ 
+def call() {
+    pipeline{
+        agent any
+        environment {
+            SONAR    = credentials('SONAR')
+            // NEXUS    = credentials('NEXUS')
+            SONARURL = "172.31.0.59"
+            // NEXUSURL = "172.31.2.247"
+        }
+        stages {
+            stage('Lint Check') {
+                steps {
+                    script {
+                        lintchecks()
+                    }
+
+                } 
+            }  
+            stage('Sonat Check') {
+                steps {
+                    script {
+                        env.ARGS="-Dsonar.sources=." 
+                        common.sonarchecks()
+                    }
+                } 
+            } 
+            stage('XYZ Check') {
+                steps {
+                    script {
+                        sh "echo xyz checks"
+                    }
+                } 
+            } 
+        } // end of stages
+    } // end of pipeline
+}
