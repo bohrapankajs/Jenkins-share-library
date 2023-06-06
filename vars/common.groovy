b81a6b5cdc09ef6a916b0df029fddd63aa4b46f4
Pankajbohra <bohrapankajs@gmail.com>
2023-06-06 15:49:52 +0530

    test

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,5 +1,5 @@
 def sonarchecks() {
-    sh "Sonarcheck started"
+    sh "echo Sonarcheck started"
 //    sh "sonar-scanner -Dsonar.host.url=http://${SONARURL}:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.login=XXXXXXXX ${ARGS}"
   //  sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} S{SONARURL} ${COMPONENT}"
     sh "echo Sonarcheck Check completed $COMPONENT"
