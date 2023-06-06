0b43005fb9e0e0303f636141adfb8b22627043f7
Pankajbohra <bohrapankajs@gmail.com>
2023-06-06 15:44:47 +0530

    test

--- vars/python.groovy
+++ vars/python.groovy
@@ -6,12 +6,12 @@ def lintchecks() {
 def call() {
     pipeline{
         agent any
-        environment {
-            SONAR    = credentials('SONAR')
-            // NEXUS    = credentials('NEXUS')
-            SONARURL = "172.31.0.59"
-            // NEXUSURL = "172.31.2.247"
-        }
+        // environment {
+        //     SONAR    = credentials('SONAR')
+        //     // NEXUS    = credentials('NEXUS')
+        //     SONARURL = "172.31.0.59"
+        //     // NEXUSURL = "172.31.2.247"
+        // }
         stages {
             stage('Lint Check') {
                 steps {
@@ -21,21 +21,21 @@ def call() {
 
                 } 
             }  
-            stage('Sonat Check') {
-                steps {
-                    script {
-                        env.ARGS="-Dsonar.sources=." 
-                        common.sonarchecks()
-                    }
-                } 
-            } 
-            stage('XYZ Check') {
-                steps {
-                    script {
-                        sh "echo xyz checks"
-                    }
-                } 
-            } 
+            // stage('Sonat Check') {
+            //     steps {
+            //         script {
+            //             env.ARGS="-Dsonar.sources=." 
+            //             common.sonarchecks()
+            //         }
+            //     } 
+            // } 
+            // stage('XYZ Check') {
+            //     steps {
+            //         script {
+            //             sh "echo xyz checks"
+            //         }
+            //     } 
+            // } 
         } // end of stages
     } // end of pipeline
 }
