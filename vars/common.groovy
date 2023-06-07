2558c7bbb8ff7341203925a311dfe6bcbed65dac
Pankajbohra <bohrapankajs@gmail.com>
2023-06-07 16:18:23 +0530

    test

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,12 +1,12 @@
 def sonarchecks() {
     sh "echo Sonarcheck started"
-   sh "sonar-scanner -Dsonar.host.url=http://${SONARURL}:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}  ${ARGS}"
-   sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONARURL} ${COMPONENT}"
+  //  sh "sonar-scanner -Dsonar.host.url=http://${SONARURL}:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}  ${ARGS}"
+  //  sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONARURL} ${COMPONENT}"
     sh "echo Sonarcheck Check completed $COMPONENT"
 }
 def mavenSonarchecks() {
     sh "echo Sonarcheck started"
-    sh "sonar-scanner -Dsonar.host.url=http://${SONARURL}:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}"
-    sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} S{SONARURL} ${COMPONENT}"
+    // sh "sonar-scanner -Dsonar.host.url=http://${SONARURL}:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}"
+    // sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} S{SONARURL} ${COMPONENT}"
     sh "echo Sonarcheck Check completed $COMPONENT"
 }
