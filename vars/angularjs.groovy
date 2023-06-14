72c3d3a788600be702fe316a5a1207376f4d70d4
Pankajbohra <bohrapankajs@gmail.com>
2023-06-14 14:28:14 +0530

    test

--- vars/angularjs.groovy
+++ vars/angularjs.groovy
@@ -1,6 +1,6 @@
 def call() {
     node {
-        // git branch: 'main', url: "https://github.com/bohrapankajs/${COMPONENT}.git"
+        git branch: 'main', url: "https://github.com/bohrapankajs/${COMPONENT}.git"
         env.APPTYPE="angularjs"
         common.lintchecks()
         env.ARGS="-Dsonar.java.binaries=target/"
